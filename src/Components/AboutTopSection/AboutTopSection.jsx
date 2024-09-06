bddbd3bb67c596ee22bc829996e2f8cb4d28beff
Ravindu Udana <ravindu@mclarens.lk>
2024-09-06 12:36:17 +0530

    small update

--- src/Components/AboutTopSection/AboutTopSection.jsx
+++ src/Components/AboutTopSection/AboutTopSection.jsx
@@ -5,7 +5,7 @@ function AboutTopSection() {
   return (
     <div className="about-top-section text-center py-5 mt-5">
       <div className="container">
-        <h1 className="about-title">About Us</h1>
+        <h1 className="about-title mt-5 mt-lg-0">About Us</h1>
         <p className="about-subtitle">Learn more about our mission, vision, and the team behind Pet Guardians.</p>
         <p className="about-description">
           At Pet Guardians, we are dedicated to providing exceptional pet care through a combination of expert advice, high-quality products, and personalized services. Our team of passionate professionals is committed to making a positive impact on the lives of pets and their owners.
--- src/Components/ScrollToTop/ScrollToTop.js
+++ src/Components/ScrollToTop/ScrollToTop.js
@@ -6,8 +6,12 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   }, [pathname]);
+  
 
   return null;
 };
